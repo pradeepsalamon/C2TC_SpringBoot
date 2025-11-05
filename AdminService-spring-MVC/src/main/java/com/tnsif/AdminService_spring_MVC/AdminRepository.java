f409b4b406969b57a01ecc37e8bb15fbaee5d68a
pradeepsalamon <pradeepsalamon14@gmail.com>
2025-11-05 10:11:28 +0530

    commit

--- AdminService-spring-MVC/src/main/java/com/tnsif/AdminService_spring_MVC/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tnsif.AdminService_spring_MVC;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-}
